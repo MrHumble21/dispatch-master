8d5ca45abce4cf774eb1770310774f071f736f1b
MrHumble21 <amalikov3@students.wiut.uz>
2022-10-25 12:24:21 +0500

    night mode btn

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,34 @@ import Description from "./components/Description/Description";
 
 import Appbar from "./components/appbar/Appbar";
 import Main from "./Main";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 function App() {
   const [dark, setDark] = useState('true');
   return (
     <>
+      {
+        dark ? <BsFillSunFill
+          onClick={() => {
+            setDark(!dark)
+            console.log(dark)
+          }}
+          className="night bg fs-1 text-warning "
+        /> :
+          <BsFillMoonStarsFill
+            onClick={() => {
+              setDark(!dark)
+              console.log(dark)
+            }}
+            className="night  fs-1 text-warning " />
 
+      }
 
-      <BsFillMoonStarsFill
-        onClick={() => {
-          setDark(!dark)
-          console.log(dark)
-        }}
-        className="night fs-2 text-warning glass" />
       <Appbar theme={dark} />
       <Routes>
         <Route path="/" element={<Main theme={dark} />}></Route>
         <Route path="/popular" element={<Popular theme={dark} />}></Route>
-        <Route path="/trending" element={<Trending  theme={dark}/>} />
-        <Route path=":id" element={<Description  theme={dark} />} />
+        <Route path="/trending" element={<Trending theme={dark} />} />
+        <Route path=":id" element={<Description theme={dark} />} />
         <Route path="/search" element={<Search theme={dark} />} />
       </Routes>
 
